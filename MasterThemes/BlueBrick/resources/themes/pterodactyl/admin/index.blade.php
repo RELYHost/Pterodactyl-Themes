aac0466681adead953c18fbb1e060dd42c28362d
Christian Clark <49704855+RELYHost@users.noreply.github.com>
2019-05-03 20:20:15 +0930

    Update index.blade.php

--- MasterThemes/BlueBrick/resources/themes/pterodactyl/admin/index.blade.php
+++ MasterThemes/BlueBrick/resources/themes/pterodactyl/admin/index.blade.php
@@ -32,9 +32,9 @@
             </div>
             <div class="box-body">
                 @if ($version->isLatestPanel())
-                    You are running Pterodactyl Panel version <code>{{ config('app.version') }}</code>. Your panel is up-to-date!
+                    The panel is currently <code>{{ config('app.version') }}</code>. No changes needed here ;)
                 @else
-                    Your panel is <strong>not up-to-date!</strong> The latest version is <a href="https://github.com/Pterodactyl/Panel/releases/v{{ $version->getPanel() }}" target="_blank"><code>{{ $version->getPanel() }}</code></a> and you are currently running version <code>{{ config('app.version') }}</code>.
+                    FUCK! The panel is outdated! Give Chris a heads up will ya.
                 @endif
             </div>
         </div>
@@ -42,17 +42,12 @@
 </div>
 <div class="row">
     <div class="col-xs-6 col-sm-3 text-center">
-        <a href="{{ $version->getDiscord() }}"><button class="btn btn-warning" style="width:100%;"><i class="fa fa-fw fa-support"></i> Get Help <small>(via Discord)</small></button></a>
+        <a href="https://rely.host/"><button class="btn btn-warning" style="width:100%;"><i class="fa fa-fw fa-support"></i>RELY website</small></button></a>
     </div>
     <div class="col-xs-6 col-sm-3 text-center">
-        <a href="https://docs.pterodactyl.io"><button class="btn btn-primary" style="width:100%;"><i class="fa fa-fw fa-link"></i> Documentation</button></a>
+        <a href="https://discord.gg/rayGxFm"><button class="btn btn-primary" style="width:100%;"><i class="fa fa-fw fa-link"></i> RELY discord</button></a>
     </div>
     <div class="clearfix visible-xs-block">&nbsp;</div>
-    <div class="col-xs-6 col-sm-3 text-center">
-        <a href="https://github.com/Pterodactyl/Panel"><button class="btn btn-primary" style="width:100%;"><i class="fa fa-fw fa-support"></i> Github</button></a>
-    </div>
-    <div class="col-xs-6 col-sm-3 text-center">
-        <a href="https://donorbox.org/pterodactyl"><button class="btn btn-success" style="width:100%;"><i class="fa fa-fw fa-money"></i> Support the Project</button></a>
-    </div>
+
 </div>
 @endsection
